971c87c8433cf8f4322a7fea0534d73697431cd0
onur <onur.kirman@ozu.edu.tr>
2021-04-02 13:32:43 +0300

    Initial commit

---
+++ src/main/java/com/finartz_case_study/airlines_ticket_system/AirlinesTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.finartz_case_study.airlines_ticket_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesTicketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesTicketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finartz_case_study/airlines_ticket_system/AirlinesTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finartz_case_study.airlines_ticket_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesTicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
