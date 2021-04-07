bd9f7a82014f1a2f7a30b331c859ec0bd246f12f
onur <onur.kirman@ozu.edu.tr>
2021-04-07 12:52:16 +0300

    Updated Ticket model

--- src/main/java/com/finartz_case_study/airlines_ticket_system/model/Ticket.java
+++ src/main/java/com/finartz_case_study/airlines_ticket_system/model/Ticket.java
@@ -1,17 +1,68 @@
 package com.finartz_case_study.airlines_ticket_system.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ticket")
 public class Ticket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id", referencedColumnName = "id")
     private Flight flight;
 
-    private int price;
+    @Column
+    private double price;
 
+    @Column(name = "credit_card_number", nullable = false, length = 16)
+    private String creditCardNumber;
 
+    public Ticket() {}
 
+    public Ticket(Flight flight, double price, String creditCardNumber) {
+        this.flight = flight;
+        this.price = price;
+        this.creditCardNumber = creditCardNumber;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
